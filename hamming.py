a0d4e41b2ccd5b4132cc1fc8e0eb8af1db34fcc4
Embo <lxmheida@gmail.com>
2017-12-18 17:00:40 +0800

    hamming distance

--- hamming.py
+++ hamming.py
@@ -1,2 +1,10 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError(".+")
+    a = list(strand_a)
+    b = list(strand_b)
+    hamming = 0
+    for i in range(len(strand_a)):
+        if a[i] != b[i]:
+            hamming += 1
+    return hamming
